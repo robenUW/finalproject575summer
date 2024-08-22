c59dfc1168e59b2efe0fae9071c192ffaec5dcb3
robenUW <112424694+robenUW@users.noreply.github.com>
2024-08-22 19:53:13 -0400

    Update bear_harvest.js

--- js/bear_harvest.js
+++ js/bear_harvest.js
@@ -6,7 +6,7 @@
 
     //chart frame dimensions
     var chartWidth = (window.innerWidth * .95) ,
-        chartHeight = 385,
+        chartHeight = 425,
         leftPadding = 33,
         rightPadding = 2,
         topBottomPadding = 4,
